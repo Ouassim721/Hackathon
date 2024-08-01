65022e1750ee696f0fdeb3265a0078b9b7347929
Ouassim721 <derjaouassim2004@gmail.com>
2024-08-01 16:19:50 +0100

    second commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Définir le moteur de template sur EJS
+app.set('view engine', 'ejs');
+
+// Définir le dossier des vues
+app.set('views', path.join(__dirname, 'views'));
+
+// Définir le dossier public pour les fichiers statiques
+app.use(express.static(path.join(__dirname, '')));
+
+// Route pour la page d'accueil
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+// Démarrer le serveur
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Serveur en cours d'exécution sur le port ${PORT}`);
+});
---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    const logo = document.getElementById('logo');
+    
+    if (window.scrollY > 50) { // Adjust this value as needed
+        navbar.classList.add('scrolled');
+        logo.src = '/images/logo1.png'; // New logo when scrolled
+    } else {
+        navbar.classList.remove('scrolled');
+        logo.src = '/images/logo2.png'; // Original logo
+    }
+});
+
+const toggleButton = document.querySelector('.navbar-toggler');
+        const navbar = document.getElementById('navbar');
+
+        toggleButton.addEventListener('click', function() {
+            navbar.classList.add('show');
+        });
+
+        // Close the menu when clicking outside
+        document.addEventListener('click', function(e) {
+            if (!navbar.contains(e.target) && !toggleButton.contains(e.target)) {
+                navbar.classList.remove('show');
+            }
+        });
